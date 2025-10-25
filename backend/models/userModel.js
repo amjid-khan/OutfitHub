07c07fce28567ea5ad2d57ce697826b3a68cb5d2
Amjid khan <amjidkurrmywal@gmail.com>
2025-10-25 18:49:20 +0500

    initial commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add a name"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please add an email"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Please add a password"],
+        },
+        role: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user",
+        },
+    },
+    { timestamps: true }
+);
+
+// Encrypt password before save
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
